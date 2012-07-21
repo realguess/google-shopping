1036a55701429e4dd7c84604eff84000b540dd3d
Chao Huang <chao@realguess.net>
2012-07-21 16:00:12 -0400

    Relaxed the parameters and added thumbnail tests.

--- test/query.js
+++ test/query.js
@@ -29,5 +29,25 @@ vows.describe('Google Shopping').addBatch({
     'should return correct item count': function (err, data) {
       assert.lengthOf(data.items, data.currentItemCount);
     },
-  }
+
+    'with thumbnails': {
+      topic: function () {
+        gs.query('Red Bull', { key: key, thumbnails: '64:64,160:*,*:120,160:120', maxResult: 1 }, this.callback);
+      },
+      'should return a thumbnail with size 64x64': function (err, data) {
+        assert.strictEqual(data.items[0].product.images[0].thumbnails[0].width, 64);
+        assert.strictEqual(data.items[0].product.images[0].thumbnails[0].height, 64);
+      },
+      'should return a thumbnail with width 160': function (err, data) {
+        assert.strictEqual(data.items[0].product.images[0].thumbnails[1].width, 160);
+      },
+      'should return a thumbnail with height 120': function (err, data) {
+        assert.strictEqual(data.items[0].product.images[0].thumbnails[2].height, 120);
+      },
+      'should return a thumbnail with size 160x120': function (err, data) {
+        assert.strictEqual(data.items[0].product.images[0].thumbnails[3].width, 160);
+        assert.strictEqual(data.items[0].product.images[0].thumbnails[3].height, 120);
+      },
+    },
+  },
 }).run();
